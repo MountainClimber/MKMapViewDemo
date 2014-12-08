ef236cf6cfbfa4f91b4fc9619744469444980592
hello <parmjit.khattra@gmail.com>
2014-12-08 23:25:19 +0530

    Initial Commit

---
+++ MKMapViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKMapViewDemo
+//
+//  Created by KHATTRA on 08/12/14.
+//  Copyright (c) 2014 xenon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MKMapViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MKMapViewDemo
+//
+//  Created by KHATTRA on 08/12/14.
+//  Copyright (c) 2014 xenon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
