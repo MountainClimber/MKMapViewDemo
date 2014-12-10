257513bb7222b8d955b987ba32bd978efd786d27
hello <parmjit.khattra@gmail.com>
2014-12-11 00:53:07 +0530

    transfer

--- MKMapViewDemo/ViewController.h
+++ MKMapViewDemo/ViewController.h
@@ -7,9 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <MKMapViewDelegate>
+//@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+- (IBAction)setMapType:(UISegmentedControl *)sender;
 
+//- (IBAction)setMaptype:(id)sender;
 
 @end
 
--- MKMapViewDemo/ViewController.m
+++ MKMapViewDemo/ViewController.m
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.mapView.mapType=MKMapTypeHybrid;
+    self.mapView.zoomEnabled=YES;
+    self.mapView.scrollEnabled=NO;
+   // NSLog(@ " % ", self.mapView.userLocation.title);
+    NSLog(@ " % ",self.mapView.userLocation.location.coordinate.latitude);
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +30,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)setMapType:(UISegmentedControl *)sender {
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            self.mapView.mapType=MKMapTypeSatellite;
+            break;
+        case 1:
+            self.mapView.mapType=MKMapTypeStandard;
+            break;
+        case 2:
+            self.mapView.mapType=MKMapTypeHybrid;
+            break;
+        default:
+            break;
+    }
+}
 @end
